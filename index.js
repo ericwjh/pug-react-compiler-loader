a867026c7543ce0b321f9aad6dbd7b6c8da385bc
吴俊宏 <499723180@qq.com>
2016-06-10 15:29:24 +0800

    use pug

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var jade = require('jade-react-compiler')
-  , path = require('path')
-  , loaderUtils = require("loader-utils");
+var pug = require('pug-react-compiler'),
+	loaderUtils = require("loader-utils");
 
-module.exports = function(source){
-  
-  var transform = "var React = require('react');\n";
-  
-  this.cacheable && this.cacheable();
-  
-  var filepath = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-  var query = loaderUtils.parseQuery(this.query);
-  return transform + jade.compileClient(source, {filename: filepath, pretty:true});
-  
-  
+module.exports = function(source) {
+
+	var transform = "var React = require('react');\n";
+
+	this.cacheable && this.cacheable();
+
+	var filepath = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+	var query = loaderUtils.parseQuery(this.query);
+	return transform + pug.compileClient(source, {
+		filename: filepath,
+		pretty: true
+	});
 }
